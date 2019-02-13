56d89b49e1ef546ec6da0fc358eea901ddc8474c
Raissa Bola <raissabola@gmail.com>
2019-02-12 18:23:40 -0800

    Assignment Intro to Express: Revised Pt. 2

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const marco = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -20,6 +20,7 @@ describe("routes : static", () => {
     it("should return status code 200", (done) => {
       request.get(marco, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("polo");
         done();
       });
     });
