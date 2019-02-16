ab5a2328ef7d5598e73a4ad7873cf7c99a8aa32d
Raissa Bola <raissabola@gmail.com>
2019-02-15 21:19:06 -0800

    Checkpoint Topic Resource Pt 1: Completed

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -3,7 +3,6 @@ const topicQueries = require("../db/queries.topics.js");
 module.exports = {
     index(req, res, next){
       topicQueries.getAllTopics((err, topics) => {
-
                 if(err){
                   res.redirect(500, "static/index");
                 } else {
