707ae6f7e85e65799accc6b35e35d2753b56448c
Raissa Bola <raissabola@gmail.com>
2019-02-22 00:39:00 -0800

    Assignment Validations: Completed

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -80,6 +80,33 @@ describe("routes : topics", () => {
         }
       );
     });
+
+    it("should not create a new topic that fails validations", (done) => {
+      const options = {
+        url: `${base}create`,
+        form: {
+
+//#1
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+
+          Topic.findOne({where: {title: "a"}})
+          .then((topic) => {
+              expect(topic).toBeNull();
+              done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
   });
 
   describe("GET /topics/:id", () => {
