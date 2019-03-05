83bb9e84c209fcbc8d5267418bcfb973872d088f
Raissa Bola <raissabola@gmail.com>
2019-03-04 20:41:05 -0800

    Assignment Voting: Completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -46,6 +46,14 @@ module.exports = (sequelize, DataTypes) => {
       });
     });
 
+    Post.afterCreate((post, callback) => {
+        return models.Vote.create({
+          value: 1,
+          userId: post.userId,
+          postId: post.id
+        });
+    });
+
     Post.prototype.getPoints = function(){
 
       // #1
