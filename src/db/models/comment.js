b3978d55e2fa8e1d494796ff3ee29d39be8b8f15
Raissa Bola <raissabola@gmail.com>
2019-03-04 21:08:46 -0800

    Checkpoint Profiles: Completed

--- src/db/models/comment.js
+++ src/db/models/comment.js
@@ -24,6 +24,22 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       onDelete: "CASCADE"
     });
+
+    Comment.addScope("lastFiveFor", (userId) => {
+
+      // #1
+          return {
+            include: [{
+              model: models.Post
+            }],
+            where: { userId: userId},
+     
+            limit: 5,
+            order: [["createdAt", "DESC"]]
+          }
+        });
+     
+
   };
   return Comment;
 };
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -46,6 +46,16 @@ module.exports = (sequelize, DataTypes) => {
       });
     });
 
+    Post.addScope("lastFiveFor", (userId) => {
+      // #2
+          return {
+            where: { userId: userId},
+      // #3
+            limit: 5,
+            order: [["createdAt", "DESC"]]
+          }
+        });
+
     Post.prototype.getPoints = function(){
 
       // #1
